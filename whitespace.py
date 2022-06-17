4364b0c4e86477da80c3c4d5d7c64b7456c7e6ce
Tray Richards <trayrichards77@gmail.com>
2022-06-16 21:02:11 -0500

    whitespace

---
+++ whitespace.py
@@ -0,0 +1,9 @@
+# Your code below:
+print("Checking the weather for you!")
+
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+  # Call the function
+weather_check()
