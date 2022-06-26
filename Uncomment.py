def320089a208759962be964aa72912c33e9a803
Tray Richards <trayrichards77@gmail.com>
2022-06-26 11:33:14 -0500

    Uncomment

---
+++ Uncomment.py
@@ -0,0 +1,16 @@
+Uncomment this when you reach the "Use the Force" section
+# train_mass = 22680
+# train_acceleration = 10
+# train_distance = 100
+# bomb_mass = 1
+
+
+# Write your code below: 
+
+Temp (C) = (Temp (F) - 32) * 5/9
+
+f100_in_celcius = f_to_c(100)
+Temp (F) = Temp (C) * (9/5) + 32
+c0_in_fahrenheit = c_to_f(0)
+
+get_force = mass * acceleration
